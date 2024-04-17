e1f1e62ca43fcfdb20a2da391589787cf432b6b2
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-17 22:50:25 +0530

    Refactor home page layout for improved mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import { Search } from "@/components";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen grainy">
+    <main className="h-screen w-screen grainy p-5 md:p-3">
       <div className="flex flex-col items-center gap-6 duration-500 animate-in fade-in-5 slide-in-from-bottom-2.5 pt-32">
-        <h1 className="text-5xl tracking-tight font-bold">⚡ Rapid Query</h1>
+        <h1 className="text-4xl md:text-5xl tracking-tight font-bold">
+          ⚡ Rapid Query
+        </h1>
 
-        <p className="max-w-prose text-zinc-600 text-lg text-center">
+        <p className="max-w-prose text-zinc-600 text-base md:text-lg text-center">
           A high-performance API built with Hono, Next.js, and Cloudflare.
-          <br />
+          <br className="hidden md:block" />
           Type a query below and get your results in milliseconds.
         </p>
 
